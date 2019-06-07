a1de40148e657982b1ed3ea3fa382cff131026d7
Ashish_Pratheesh <ashishpradeesh@gmail.com>
2019-06-06 21:03:48 -0400

    initial commit

---
+++ day1sample/main.swift
@@ -0,0 +1,34 @@
+//
+//  main.swift
+//  day1sample
+//
+//  Created by MacStudent on 2019-06-06.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var a:Int
+var b:Int
+var c:Int
+
+
+a=100
+b=20
+
+
+c = a + b
+print ("sum is",c)
+
+
+c = a-b
+print ("sub", c)
+
+c = a*b
+print ( c)
+c=a/b
+print (c)
+
+
